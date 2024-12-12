4a2923beb50c50f4523079edcc8eb240aa569580
uShire <afgregana@up.edu.ph>
2024-12-12 08:29:48 +0800

    updated

--- src/componets/pages/frontend/MenuList.jsx
+++ src/componets/pages/frontend/MenuList.jsx
@@ -1,9 +1,10 @@
 import { imgPath } from "@/componets/helpers/functions-general";
 import React from "react";
 import { menus } from "../backend/menu-data";
+import useQueryData from "@/componets/custom-hook/useQueryData";
 
 const MenuList = ({category, cartData,setCartData, setIsSuccess}) => {
-  const menuFilter = menus.filter((item) => item.menu_category === category);
+  const menuFilter = menus.filter((item) => item.food_category_id === category);
 
   const handleAdd = (item) => {
     const exist = cartData.find((data) => data.menu_aid === item.menu_aid);
@@ -21,15 +22,24 @@ const MenuList = ({category, cartData,setCartData, setIsSuccess}) => {
     setIsSuccess(true);
   };
 
-
+  const {
+    isFetching,
+    error,
+    data: result,
+    status,
+  } = useQueryData(
+    `/v2/food`, //enpoint
+    "get", //method
+    "food" //key
+  );
 
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
 
-      {menuFilter.map((item, key) => (<button key={key} onClick={() => handleAdd(item)}>
-        <img src={`${imgPath}/${item.menu_img}`} alt="" className="w-[80%] mx-auto mb-2" />
-        <h6 className="font-bold text-sm">{item.menu_title}</h6>
-        <p className="text-sm">P {item.menu_price}</p>
+      {result?.count > 0 && result.data.map((item, key) => (<button key={key} onClick={() => handleAdd(item)}>
+        <img src={`${imgPath}/${item.food_image}`} alt="" className="w-[80%] mx-auto mb-2" />
+        <h6 className="font-bold text-sm">{item.food_title}</h6>
+        <p className="text-sm">P {item.food_price}</p>
       </button>))}
 
       
